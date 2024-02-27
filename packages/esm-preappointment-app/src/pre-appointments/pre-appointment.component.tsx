bd925d6f5558d9f8e6b6d5f752c659772601802d
Justus446 <omondijustus056@gmail.com>
2024-02-27 09:39:51 +0300

    filters to dashboard

--- packages/esm-preappointment-app/src/pre-appointments/pre-appointment.component.tsx
+++ packages/esm-preappointment-app/src/pre-appointments/pre-appointment.component.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react';
+/* eslint-disable no-console */
 import {
   DataTable,
   TableContainer,
@@ -10,93 +10,188 @@ import {
   TableCell,
   DataTableSkeleton,
   Pagination,
-  Dropdown,
-  Button,
 } from '@carbon/react';
-import { usePreAppointments, usePreAppointmentsFilter, weeksInYear } from './pre-appointment.resource';
+import React from 'react';
+import { usePreAppointments, weeksInYear } from './pre-appointment.resource';
 import { useTranslation } from 'react-i18next';
 import { ErrorState, usePagination } from '@openmrs/esm-framework';
 import { usePaginationInfo } from '@openmrs/esm-patient-common-lib';
 import styles from './pre-appointment.scss';
-
+import { Row } from '@carbon/react';
+import { Dropdown } from '@carbon/react';
+import { Button } from '@carbon/react';
 // Define the type of each row in the table
 interface TableRow {
-  id: string;
+  id: any;
   ccc_number: string;
   person_name: string;
+  ovcid_id: string;
+  upi_number: string;
+  program: string;
+  predicted_risk: string;
+  predicted_prob_disengage: string;
+  prediction_generated_date: string;
+  sms_delivery_status: string;
+  rtc_date: string;
+  follow_up_type: string;
+  follow_up_reason: string;
+  rescheduled_date: string;
+  contact_reached: string;
+  attempted_home_visit: string;
+  reason_not_attempted_home_visit: string;
+  was_client_found: string;
+  comments: string;
+  reason_client_not_found: string;
+  was_follow_up_successful: boolean;
+  number_of_failed_phone_attempts: string;
+  gender: string;
+  identifiers: string;
+  phone_number: string;
+  latest_rtc_date: string;
+  latest_vl: number;
+  vl_category: string;
+  latest_vl_date: string;
+  last_appointment: string;
+  cur_meds: string;
+  nearest_center: string;
+  covid_19_vaccination_status: string;
   age: number;
   uuid: string;
 }
-
 type PreAppointmentProps = {};
-
 const PAGE_SIZE = 10;
+// Should be provided by report filter controls
+const testProps = {
+  locationUuid: '08feae7c-1352-11df-a1f1-0026b9348838',
+  yearWeek: '2024-W07',
+};
+const weeksInYearData = weeksInYear();
 
 export const PreAppointment: React.FC<PreAppointmentProps> = () => {
-  const [locationUuid, setLocationUuid] = useState('');
-  const [yearWeek, setYearWeek] = useState('');
-  const [outcomeSuccess, setOutcomeSuccess] = useState(1);
-  const PatientAppointmentContext = createContext(null);
   const { t } = useTranslation();
-  const { preappointments, isLoading, error } = usePreAppointments(locationUuid, yearWeek);
-  const weeksInYearData = weeksInYear(); // Renamed to avoid naming conflict
-
-  // Define the headers for the table
+  const { preappointments, isLoading, error } = usePreAppointments(testProps.locationUuid, testProps.yearWeek);
+  // TODO: Append the rest of table headers for the pre-appointments
   const headers = [
     { key: 'ccc_number', header: 'CCC Number' },
     { key: 'person_name', header: 'Person Name' },
+    { key: 'upi_number', header: 'Nupi_number' },
+    { key: 'gender', header: 'Gender' },
     { key: 'age', header: 'Age' },
+    { key: 'identifiers', header: 'Identifiers' },
+    { key: 'program', header: 'Program' },
+    { key: 'phone_number', header: 'Phone Number' },
+    { key: 'predicted_risk', header: 'Predicted Risk' },
+    { key: 'predicted_prob_disengage', header: 'Predicted Score(%)' },
+    { key: 'prediction_generated_date', header: 'Prediction Generated Date' },
+    { key: 'last_appointment', header: 'latest Appointment' },
+    { key: 'rtc_date', header: 'Rtc Date' },
+    { key: 'latest_rtc_date', header: 'Latest Rtc Date' },
+    { key: 'cur_meds', header: 'Current Regimen' },
+    { key: 'latest_vl', header: 'Latest VL' },
+    { key: 'latest_vl_date', header: 'Latest VL Date' },
+    { key: 'vl_category', header: 'VL Category' },
+    { key: 'follow_up_type', header: 'Follow-up Type' },
+    { key: 'follow_up_reason', header: 'Follow-up Reason' },
+    { key: 'was_follow_up_successful', header: 'Follow-up Success' },
+    { key: 'rescheduled_date', header: 'Rescheduled Date' },
+    { key: 'number_of_failed_phone_attempts', header: 'No. of Failed Phone Attempts' },
+    { key: 'comments', header: 'comments' },
+    { key: 'sms_delivery_status', header: 'SMS outcome' },
+    { key: 'contact_reached', header: 'Contact Reached' },
+    { key: 'attempted_home_visit', header: 'Attempted Home Visit' },
+    { key: 'reason_not_attempted_home_visit', header: 'Reason Not Attempted Home Visit' },
+    { key: 'was_client_found', header: 'Client Found' },
+    { key: 'reason_client_not_found', header: 'Reason Client Not Found' },
+    { key: 'nearest_center', header: 'Nearest Center' },
+    { key: 'covid_19_vaccination_status', header: 'COVID-19 Vaccination Status' },
   ];
-
-  // Pagination logic
   const { results, goTo, currentPage } = usePagination(preappointments ?? [], PAGE_SIZE);
   const { pageSizes } = usePaginationInfo(PAGE_SIZE, preappointments.length, currentPage, results.length);
 
-  // Map data for table rows
+  console.log(weeksInYear());
+
   const tableRows: TableRow[] = results.map((row: any) => ({
     id: `${row.person_id}`,
     ccc_number: row.ccc_number,
     person_name: row.person_name ?? '--',
     age: row.age,
     uuid: row.uuid,
+    ovcid_id: row.ovcid_id,
+    upi_number: row.upi_number,
+    program: row.program,
+    predicted_risk: row.predicted_risk,
+    predicted_prob_disengage: row.predicted_prob_disengage,
+    prediction_generated_date: row.prediction_generated_date,
+    sms_delivery_status: row.sms_delivery_status,
+    rtc_date: row.rtc_date,
+    follow_up_type: row.follow_up_type,
+    follow_up_reason: row.follow_up_reason,
+    rescheduled_date: row.rescheduled_date,
+    contact_reached: row.contact_reached,
+    attempted_home_visit: row.attempted_home_visit,
+    reason_not_attempted_home_visit: row.reason_not_attempted_home_visit,
+    was_client_found: row.was_client_found,
+    comments: row.comments,
+    reason_client_not_found: row.reason_client_not_found,
+    was_follow_up_successful: row.was_follow_up_successful,
+    number_of_failed_phone_attempts: row.number_of_failed_phone_attempts,
+    gender: row.gender,
+    identifiers: row.identifiers,
+    phone_number: row.phone_number,
+    latest_rtc_date: row.latest_rtc_date,
+    latest_vl: row.latest_vl,
+    vl_category: row.vl_category,
+    latest_vl_date: row.latest_vl_date,
+    last_appointment: row.last_appointment,
+    cur_meds: row.cur_meds,
+    nearest_center: row.nearest_center,
+    covid_19_vaccination_status: row.covid_19_vaccination_status,
   }));
-
-  // Function to handle generating the patient list
-  const handleGenerateList = () => {
-    const { preappointments, isLoading, error } = usePreAppointmentsFilter(locationUuid, yearWeek, outcomeSuccess);
-    return { preappointments, isLoading, error };
-  };
 
   if (isLoading) {
     return <DataTableSkeleton headers={headers} aria-label={t('preAppointments', 'Pre appointments')} />;
   }
-
   if (error) {
     return <ErrorState error={error} headerTitle={t('preAppointments', 'Pre appointments')} />;
   }
-
   return (
     <div className={styles.preAppointment}>
       <Dropdown
-        items={weeksInYearData} // Updated to use the weeksInYearData variable
-        itemToString={(item) => (item ? item.text : '')}
-        titleText="Select Week"
-        onChange={({ selectedItem }) => setYearWeek(selectedItem.id)}
-        selectedItem={yearWeek}
+        items={[
+          { id: 'option-0', text: 'MTRH' },
+          { id: 'option-1', text: 'Test Site ' },
+          { id: 'option-2', text: 'Webuye' },
+        ]}
+        itemToElement={(item) => (item ? <span className="test">{item.text}</span> : '')}
+        label="Select Location"
+        titleText="Select Location"
       />
       <Dropdown
         items={[
-          { id: 'option-0', text: 'All' },
-          { id: 'option-1', text: 'Follow-up Successful' },
-          { id: 'option-2', text: 'Failed Follow-up Attempt' },
-          { id: 'option-3', text: 'Follow-up not Attempted' },
+          { id: '2024-W06', text: '2024 W06-From Mon Jan 29 2024 to Sun Feb 04 2024' },
+          { id: '2024-W07', text: '2024 W07-From Mon Feb 05 2024 to Sun Feb 11 2024' },
+          { id: '2024-W08', text: '2024 W08-From Mon Feb 12 2024 to Sun Feb 18 2024' },
+          { id: '2024-W09', text: '2024 W09-From Mon Feb 19 2024 to Sun Feb 25 2024' },
+          { id: '2024-W09', text: '2024 W09-From Mon Feb 26 2024 to Sun Mar 03 2024' },
         ]}
-        itemToString={(item: { text: any }) => (item ? item.text : '')}
-        titleText="Appointment"
-        onChange={({ selectedItem }) => setOutcomeSuccess(selectedItem.id)}
-        selectedItem={outcomeSuccess}
+        itemToElement={(item) => (item ? <span className="test">{item.text}</span> : '')}
+        label="Select Week"
+        titleText="Year Week"
       />
-      <Button onClick={handleGenerateList}>Generate Patient List</Button>
+
+      <Dropdown
+        items={[
+          { id: 'option-0', text: 'All' },
+          { id: 'option-1', text: 'Appointment successful' },
+          { id: 'option-2', text: 'Appointment Failed' },
+        ]}
+        itemToElement={(item) => (item ? <span className="test">{item.text}</span> : '')}
+        label="Filter"
+        titleText="Filter Type"
+      />
+
+      <Button>Generate Patient List</Button>
+
       <DataTable rows={tableRows} headers={headers} size="sm" useZebraStyles>
         {({ rows, headers, getHeaderProps, getRowProps, getTableProps, getTableContainerProps }) => (
           <TableContainer
--- packages/esm-preappointment-app/src/pre-appointments/pre-appointment.resource.tsx
+++ packages/esm-preappointment-app/src/pre-appointments/pre-appointment.resource.tsx
@@ -1,6 +1,5 @@
 import { type PreAppointmentsConfig } from '../config-schema';
 import { useConfig } from '@openmrs/esm-framework';
-const moment = require('moment');
 
 // Should use openmrsFetch to fetch data from the backend
 // import { openmrsFetch } from '@openmrs/esm-api';
@@ -11,7 +10,6 @@ import { Buffer } from 'buffer';
 const username = 'fkamau';
 const password = 'Th1ng1r@1';
 const basicAuthBase64 = Buffer.from(`${username}:${password}`).toString('base64');
-const this_year = moment().year();
 const fetcher = async (url) => {
   try {
     const response = await fetch(url, {
@@ -25,41 +23,70 @@ const fetcher = async (url) => {
     throw new Error(`An error occurred while fetching data: ${error.message}`);
   }
 };
-export const usePreAppointments = (locationUuid: string, yearWeek: string) => {
-  const url = `https://ngx.ampath.or.ke/etl-latest/etl/ml-weekly-predictions?locationUuids=${locationUuid}&yearWeek=${yearWeek}`;
+export const usePreAppointments = (locationUuid: string, yearWeek: string, successCode?: string) => {
+  let url = `https://ngx.ampath.or.ke/etl-latest/etl/ml-weekly-predictions?locationUuids=${locationUuid}&yearWeek=${yearWeek}`;
+
+  if (successCode && successCode !== '0') {
+    url += `&appointmentSuccess=${successCode}`;
+  }
+
   const { data, error, isValidating } = useSWR(url, fetcher);
+
   return { preappointments: data?.result ?? [], isLoading: !data && !error, error, isValidating };
 };
 
-export const usePreAppointmentsFilter = (locationUuid: string, yearWeek: string, outcomeSuccess: number) => {
-  const url = `https://ngx.ampath.or.ke/etl-latest-beta/etl/ml-weekly-predictions?locationUuids=${locationUuid}&yearWeek=${yearWeek}&successfulOutcome=${outcomeSuccess}`;
-
-  const { data, error, isValidating } = useSWR(url, fetcher);
-  return { preappointments: data?.result ?? [], isLoading: !data && !error, error, isValidating };
+// Helper function to get ISO year
+const getISOYear = (date) => {
+  const year = date.getFullYear();
+  const week1 = new Date(year, 0, 4); // January 4th is always in week 1
+  return year + (date.getTime() < week1.getTime() ? -1 : 0);
 };
 
+// Helper function to get ISO week number
+const getISOWeek = (date) => {
+  const onejan = new Date(date.getFullYear(), 0, 1);
+  return Math.ceil(((date.getTime() - onejan.getTime()) / 86400000 + onejan.getDay() + 1) / 7);
+};
+
+// Helper function to get the start of the ISO week
+const getStartOfWeek = (date) => {
+  const diff = date.getDate() - date.getDay() + (date.getDay() === 0 ? -6 : 1);
+  const startOfWeek = new Date(date.setDate(diff));
+  return startOfWeek.toDateString();
+};
+
+// Helper function to get the end of the ISO week
+const getEndOfWeek = (date) => {
+  const diff = date.getDate() - date.getDay() + (date.getDay() === 0 ? -6 : 1);
+  const endOfWeek = new Date(date.setDate(diff + 6));
+  return endOfWeek.toDateString();
+};
 export const weeksInYear = () => {
-  let startdate = new Date(`${this_year - 1}-1-1`);
-  let enddate = moment(new Date()).format('YYYY-MM-DD');
+  const today = new Date();
+  const this_year = today.getFullYear();
+  let startdate = new Date(this_year - 1, 0, 1); // January 1st of last year
+  let enddate = new Date(today.getFullYear(), today.getMonth(), today.getDate()); // Current date
+
   const weeks = [];
-  while (new Date(startdate).getTime() <= new Date(enddate).getTime()) {
-    const week = moment(startdate).format('W');
-    let startofweek = moment(startdate).startOf('isoWeek').format('ll');
-    let endofweek = moment(startdate).endOf('isoWeek').format('ll');
+
+  while (startdate.getTime() <= enddate.getTime()) {
+    const week = getISOWeek(startdate);
+    const startofweek = getStartOfWeek(startdate);
+    const endofweek = getEndOfWeek(startdate);
 
     weeks.push({
-      id: `${moment(startdate).startOf('isoWeek').year()}-W${('0' + week).slice(-2)}`,
-      text: `${moment(startdate).startOf('isoWeek').year()} W${('0' + week).slice(
-        -2,
-      )}-From ${startofweek} to ${endofweek}`,
+      id: `${getISOYear(startdate)}-W${('0' + week).slice(-2)}`,
+      text: `${getISOYear(startdate)} W${('0' + week).slice(-2)}-From ${startofweek} to ${endofweek}`,
     });
-    startdate = moment(startdate).add(7, 'days').format('YYYY-MM-DD');
+
+    startdate.setDate(startdate.getDate() + 7); // Move to the next week
   }
+
   weeks.push({
-    value: `${moment(enddate).startOf('isoWeek').year()}-W${('0' + moment(enddate).format('W')).slice(-2)}`,
-    label: `${moment(enddate).startOf('isoWeek').year()} W${('0' + moment(enddate).format('W')).slice(
-      -2,
-    )}-From ${moment(enddate).startOf('isoWeek').format('ll')} to ${moment(enddate).endOf('isoWeek').format('ll')}`,
+    id: `${getISOYear(enddate)}-W${('0' + getISOWeek(enddate)).slice(-2)}`,
+    text: `${getISOYear(enddate)} W${('0' + getISOWeek(enddate)).slice(-2)}-From ${getStartOfWeek(
+      enddate,
+    )} to ${getEndOfWeek(enddate)}`,
   });
 
   return weeks;
